23c29f3e26ee85cfe87df7fc87c4aa17d0b6b17a
nilson lagos <nilson@muuklabs.com>
2019-07-18 13:42:38 -0500

    a

--- utils.js
+++ utils.js
@@ -12,8 +12,8 @@ function exportAndSaveCanvas(element, step='aaaa', test='bbbb')  {
   window.scrollTo(rect.left, rect.top);
   html2canvas(document.body , { 
   // type: 'view',
-  // x: rect.left + window.scrollX,
-  // y: rect.top + window.scrollY,
+  x: rect.left,
+  y: rect.top,
   width: window.innerWidth,
   height: window.innerHeight,
   // useCORS: true,
